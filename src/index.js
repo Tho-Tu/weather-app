cffc68b105d11c6b7c26a335900d0aae80b98d69
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-10-05 22:13:02 +1000

    Add query select to DOM elements

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const weatherAPIKey = "07e4e2a749f046b5afc04637230210";
 
-getWeather("brisbane", weatherAPIKey);
-getForecast("brisbane", weatherAPIKey);
+// getWeather("new york", weatherAPIKey);
+// getForecast("new york", weatherAPIKey);
+
+searchCity();
 
 async function getWeather(location, apiKey) {
   // current weather
@@ -31,6 +33,40 @@ async function getForecast(location, apiKey) {
     return responseData;
   } catch (err) {
     console.log(err);
+  }
+}
+
+function searchCity() {
+  const weatherLocation = document.getElementById("weather-location");
+  const searchWeather = document.getElementById("search-weather");
+  searchWeather.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    getWeather(`${weatherLocation.value}`, weatherAPIKey);
+  });
+
+  // HANDLE ERROR
+}
+
+function toggleTemperature() {
+  const toggleTemp = document.getElementById("toggle-temp");
+}
+
+function displayDOM() {
+  // today's weather
+  const locationName = document.querySelector(".location-name");
+  const locationTime = document.querySelector(".location-time");
+  const locationWeather = document.querySelector("location-weather");
+  const locationTemperature = document.querySelector(".location-temperature");
+  const locationFeel = document.querySelector(".location-feel");
+  const locationWind = document.querySelector(".location-wind");
+  const locationHumidity = document.querySelector(".location-humidity");
+
+  // weather forecast for next 3 days
+  for (let i = 1; i < 4; i++) {
+    const dayName = document.querySelector(`[data-name="${i}"]`);
+    const dayWeather = document.querySelector(`[data-name="${i}"]`);
+    const dayTemp = document.querySelector(`[data-name="${i}"]`);
   }
 }
 
